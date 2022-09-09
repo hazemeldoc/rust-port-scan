b32cb4cb83da3300be7829f0f6cec614e606a10e
hazemeldoc <ahmed20191704004@cis.asu.edu.eg>
2022-09-09 13:57:57 +0200

    commit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::vec::Vec;
 use tokio::time::sleep;
 use tokio::net::TcpStream;
 use clap::{App, Arg};
+
+mod portscan;
 #[tokio::main]
 async fn main() {
 
@@ -32,7 +34,7 @@ async fn main() {
     for l in 0..66
     {
         let handle_parent = tokio::spawn(async move{
-        if start_scan_1000(l,octet1,octet2,octet3,octet4).await==true
+        if portscan::start_scan_1000(l,octet1,octet2,octet3,octet4).await==true
         {
            //still workin on it
         }
@@ -47,39 +49,3 @@ async fn main() {
     }
 
 }
-async fn start_scan_1000 (l:u16,oct1:u8,oct2:u8,oct3:u8,oct4:u8)-> bool
-{
-    let mut handles=vec![];
-        
-    for i in 0..1001
-    {
-        let handle = tokio::spawn(async move{
-        if test_port(oct1,oct2,oct3,oct4,i+(l*1000)).await == true
-        {
-            println!("port {} is open",i+(l*1000));
-        }
-        });
-        if i+(l*1000) >= 65535{
-            return true;
-        }
-        handles.push(handle);
-    }
-    
-    for handle in handles
-    {
-        handle.await.unwrap();
-    }
-
-    sleep(Duration::from_millis(300)).await;
-    return false;
-}
-async fn test_port(oct1:u8,oct2:u8,oct3:u8,oct4:u8,port:u16) -> bool
-{       
-    let addr=SocketAddr::from(([oct1,oct2,oct3,oct4],port));
-    let time_out=Duration::new(3    ,0);
-    match tokio::time::timeout(time_out, TcpStream::connect(addr)).await {
-        Ok(Ok(_)) => return true,
-        _ => {}
-    }
-    return false;
- }
---
+++ src/portscan.rs
@@ -0,0 +1,43 @@
+use std::net::{SocketAddr};
+use std::time::Duration;
+use std::vec::Vec;
+use tokio::time::sleep;
+use tokio::net::TcpStream;
+use clap::{App, Arg};
+
+pub async fn start_scan_1000 (l:u16,oct1:u8,oct2:u8,oct3:u8,oct4:u8)-> bool
+{
+    let mut handles=vec![];
+        
+    for i in 0..1001
+    {
+        let handle = tokio::spawn(async move{
+        if test_port(oct1,oct2,oct3,oct4,i+(l*1000)).await == true
+        {
+            println!("port {} is open",i+(l*1000));
+        }
+        });
+        if i+(l*1000) >= 65535{
+            return true;
+        }
+        handles.push(handle);
+    }
+    
+    for handle in handles
+    {
+        handle.await.unwrap();
+    }
+
+    sleep(Duration::from_millis(300)).await;
+    return false;
+}
+async fn test_port(oct1:u8,oct2:u8,oct3:u8,oct4:u8,port:u16) -> bool
+{       
+    let addr=SocketAddr::from(([oct1,oct2,oct3,oct4],port));
+    let time_out=Duration::new(3    ,0);
+    match tokio::time::timeout(time_out, TcpStream::connect(addr)).await {
+        Ok(Ok(_)) => return true,
+        _ => {}
+    }
+    return false;
+ }
