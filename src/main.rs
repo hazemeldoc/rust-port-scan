aa2c9f9a79ee5c97dc48b49b2f170a342f2ba011
hazemeldoc <ahmed20191704004@cis.asu.edu.eg>
2022-09-09 14:46:50 +0200

    commmit

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
+use std::env;
 use std::net::{SocketAddr};
+use std::process::exit;
 use std::time::Duration;
 use std::vec::Vec;
 use tokio::time::sleep;
 use tokio::net::TcpStream;
 use clap::{App, Arg};
 
+mod file_read;
 mod portscan;
 #[tokio::main]
 async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+    let mut Q=vec![];
+    file_read::read_file_line_by_line(path, &mut Q);
+    for i in 0..Q.len()
+    {
+        let target=Q.pop().unwrap();
+        let split = target.split(".");
+        let vec = split.collect::<Vec<&str>>();
+	    let octet1=vec[0].parse::<u8>().unwrap();
+        let octet2=vec[1].parse::<u8>().unwrap();
+        let octet3=vec[2].parse::<u8>().unwrap();
+        let octet4=vec[3].parse::<u8>().unwrap();
+        start_scan(octet1,octet2,octet3,octet4).await;
+    }
+}
 
-    let param = App::new(clap::crate_name!())
-        .version(clap::crate_version!())
-        .about(clap::crate_description!())
-        .arg(
-            Arg::with_name("target")
-                .help("The target to scan")
-                .required(true)
-                .index(1),
-        )
-        .setting(clap::AppSettings::ArgRequiredElseHelp)
-        .setting(clap::AppSettings::VersionlessSubcommands)
-        .get_matches();
-
-    let target=param.value_of("target").unwrap();
-    let split = target.split(".");
-    let vec = split.collect::<Vec<&str>>();
-	let octet1=vec[0].parse::<u8>().unwrap();
-    let octet2=vec[1].parse::<u8>().unwrap();
-    let octet3=vec[2].parse::<u8>().unwrap();
-    let octet4=vec[3].parse::<u8>().unwrap();
-
+async fn start_scan(octet1:u8,octet2:u8,octet3:u8,octet4:u8)
+{
     let mut handles_parent=vec![];
     for l in 0..66
     {
@@ -47,5 +47,4 @@ async fn main() {
         sleep(Duration::from_millis(200)).await;
         handle_parent.await.unwrap();
     }
-
 }
